576d2c2d3222486251449161f475cae8034423d7
Madhur Prashant <129979633+madhurprash@users.noreply.github.com>
2025-02-03 16:50:26 -0500

    first working version

--- generated_code.py
+++ generated_code.py
@@ -1,12 +1,14 @@
-Here is the Python code to upload files to Amazon S3 using the `boto3` library:
 ```python
+# Import necessary modules from the AWS SDK
 import boto3
+from botocore.exceptions import NoCredentialsError
 
-def upload_file_to_s3(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
+def upload_file_to_s3(file_name, bucket_name, object_name=None):
+    """
+    Upload a file to an S3 bucket.
 
     :param file_name: File to upload
-    :param bucket: Bucket to upload to
+    :param bucket_name: Bucket to upload to
     :param object_name: S3 object name. If not specified then file_name is used
     :return: True if file was uploaded, else False
     """
@@ -14,11 +16,11 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
     if object_name is None:
         object_name = file_name
 
-    # Upload the file
+    # Initialize a session using Amazon S3
     s3_client = boto3.client('s3')
+
     try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+        # Upload the file
+        s3_client.upload_file(file_name, bucket_name, object_name)
+        print(f"File {file_name} uploaded successfully to {bucket_name}/{object_name}")
+        return True
