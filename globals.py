ab31ef14883b82409b482479d31d5a4f7ef48d3b
Madhur Prashant <129979633+madhurprash@users.noreply.github.com>
2025-02-03 15:15:43 -0500

    initial commit

--- globals.py
+++ globals.py
@@ -1,3 +1,4 @@
+import os
 # global constants
 CONFIG_FILE: str = "config.yml"
 MODEL_ID_TOPROMPT_ID_MAPPING_FILE: str = ".model_id_to_prompt_id_mapping.json"
@@ -8,3 +9,6 @@ CODE_GEN_LAMBDA: str = "code-gen"
 LAMBDA_ARN_FILE: str = ".lambda_arn"
 
 CODE_GEN_MODEL_ID: str = "amazon.nova-lite-v1:0"
+DATA_DIR: str = "knowledge"
+API_SPEC_FILE: str = "openapi.json"
+TITAN_TEXT_EMBED_V2: str = 'amazon.titan-embed-text-v2:0'
